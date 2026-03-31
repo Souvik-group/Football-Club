ba572e193693edb1e9a66aeeee3daf567c36c5da
souvik patra <souvikpatra066@gmail.com>
2026-03-31 09:52:42 +0530

    fix: render deployment setup

--- football_club/settings.py
+++ football_club/settings.py
@@ -29,7 +29,7 @@ import os
 
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1', 'localhost','*']
 
 
 # Application definition
